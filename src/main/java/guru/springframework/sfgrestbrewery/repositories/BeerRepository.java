301e5f0e049e0186aa0b3681f3c2556b0779c1c6
nthY <n_hat.yalcin@outlook.com>
2023-10-25 00:28:32 +0300

    *Change the resource path for beers to "/beer". Spring data rest uses plural naming "/beers"
    *Change name of list in JSON to "beerList".
    By default, it was "beers"

--- src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
@@ -6,10 +6,18 @@ import guru.springframework.sfgrestbrewery.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+/*
+    By default, Spring Data Rest provides a path related to beer entity as ".../beers".
+    But, say that we want to make it ".../beer". So, we need to change path.
 
+    Besides, the property name of the returned list of beers is "beers: [....]".
+    Say that we want to make it "beerList". Then, change collectionResourceRel.
+ */
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beerList")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
